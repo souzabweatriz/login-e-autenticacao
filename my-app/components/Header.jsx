b99d9e10a784c5b2799eef3f41dc7dbeec8c3086
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2025-03-27 10:40:12 -0300

    Update Header.jsx

--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.links}>
-                <li className={styles.list}> <a className={styles.list} href="/signin">Entrar</a></li>
-                <li className={styles.list}> <a className={styles.list} href="/profile">Sobre</a></li>
-                <li className={styles.list}> <a className={styles.list} href="/signup">Cadastre-se</a></li>
+                <li className={styles.list}> <a href="/signin">Entrar</a></li>
+                <li className={styles.list}> <a href="/profile">Sobre</a></li>
+                <li className={styles.list}> <a href="/signup">Cadastre-se</a></li>
             </div>
         </header>)
 }
